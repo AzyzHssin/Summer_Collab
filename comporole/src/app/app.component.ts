46f5fc871eee7fe57db0dfb36d0b90d42e137693
unknown <adib.baldi@etudiant-fst.utm.tn>
2023-06-13 18:36:33 +0200

    second push

--- comporole/src/app/app.component.ts
+++ comporole/src/app/app.component.ts
@@ -8,8 +8,19 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'comporole';
   @ViewChild('descriptionSection') descriptionSectionRef!: ElementRef;
-
+  @ViewChild('ActSection') actSectionRef!: ElementRef;
+  @ViewChild('contactSection') conSectionRef!: ElementRef;
+  @ViewChild('solSection') solSectionRef!: ElementRef;
   scrollToDescription() {
     this.descriptionSectionRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+  scrollTocontact() {
+    this.conSectionRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+  scrollTosolution() {
+    this.solSectionRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+  scrollTosAct() {
+    this.actSectionRef.nativeElement.scrollIntoView({ behavior: 'smooth' });
   }
 }
